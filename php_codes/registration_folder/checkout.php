c77b58697f09257bd3ce9c9a74493e68a7bf454f
martynapawlus <pawlusmartyna1@gmail.com>
2020-08-21 18:10:48 +0200

    deleting php

--- php_codes/registration_folder/checkout.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "server name: " . $_SERVER['SERVER_NAME']."<br>";
-echo "server address : " . $_SERVER['SERVER_ADDR'] . "<br>";
-echo "server port : " . $_SERVER['SERVER_PORT'] . "<br>";
-echo "document root : " . $_SERVER['DOCUMENT_ROOT'] . "<br>";
-?>
